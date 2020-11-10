a50e44abd9c606a4085c9e72183d2cd04a9c88ad
Aoze Xie <jansonjoanxie@gmail.com>
2020-11-10 23:03:02 +0800

    搭建movie、cinema静态页面

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <Header title="我的"/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  name: 'Mine',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
